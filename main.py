a9d9cde87bba7f298b411e2a5ca3b780179bb651
Hy Vuong <vuonghy2442@hotmail.com.vn>
2019-12-05 10:40:59 +0700

    first new interpreter with debug

--- main.py
+++ main.py
@@ -34,16 +34,6 @@ def substitute(term, subs):
 def lsubstitute(lterm, subs):
     return [substitute(x, subs) for x in  lterm]
 
-def subs_to_dict(subs):
-    d = {}
-    for x, y in zip(*subs):
-        assert(is_var(x))
-        d[x.name] = y
-    return d
-
-def subs_to_set(subs):
-    return set([x.name for x in subs[0]])
-
 def term_equal(p, q):
     if p.name != q.name or len(p.arg) != len(q.arg):
         return False
@@ -65,70 +55,75 @@ def has_variable(term, var_name):
     return False
 
 #two list of equation :))
-def unify(eq1, eq2, done_var = set()):
+#simple unify, not fully replaced all term, and can be contradictory
+#guarantee to return a dict
+def simple_unify(eq1, eq2, subs):
     assert(len(eq1) == len(eq2))
 
-    if len(eq1) == 0:
-        return []
-
-    change = True
-
-    while change:
-        change = False
-
-        new_eq1 = []
-        new_eq2 = []
-        for i in range(len(eq1)):
-            t1 = eq1[i]
-            t2 = eq2[i]
-            #delete
-            if term_equal(t1, t2):
-                continue
-
-            #decompose
-            if not is_var(t1) and not is_var(t2):
-                if t1.name != t2.name or len(t1.arg) != len(t2.arg):
-                    #conflict
-                    return None
-                else:
-                    change = True
-                    new_eq1 += t1.arg
-                    new_eq2 += t2.arg
-                    continue
+    for i in range(len(eq1)):
+        t1 = eq1[i]
+        t2 = eq2[i]
         
-            #swap
-            if not is_var(t1) and is_var(t2):
+        if term_equal(t1, t2):
+            continue
+
+        if not is_var(t1) and not is_var(t2):
+            if t1.name != t2.name or len(t1.arg) != len(t2.arg):
+                #conflict
+                return False
+            else:
+                if not simple_unify(t1.arg, t2.arg, subs):
+                    return False
+        else:
+            if is_var(t2):
                 t = t1
                 t1 = t2
                 t2 = t
 
-            #eliminate
-            if is_var(t1):
-                #check if recursive
-                if has_variable(t2, t1.name):
-                    return None #since we check if they are equal in the first
-                elif t1.name not in done_var:
-                    change = True
-                    done_var.add(t1.name)
+            if t1.name in subs:
+                if not simple_unify([subs[t1.name]], [t2], subs):
+                    return False
+            else:
+                subs[t1.name] = t2
 
-                    new_eq1 += eq1[i + 1:]
-                    new_eq2 += eq2[i + 1:]
-                    
-                    new_eq1 = lsubstitute(new_eq1, {t1.name : t2})
-                    new_eq2 = lsubstitute(new_eq2, {t1.name : t2})
+    return True
 
-                    new_eq1.append(t1)
-                    new_eq2.append(t2)
-                else:
-                    #done var is always at the end
-                    new_eq1 += eq1[i:]
-                    new_eq2 += eq2[i:]
-                break
+#replacify
+def remove_ref(subs):
+    def remove_ref_term(term, d, done, stack):
+        if is_var(term):
+            if term.name in stack:
+                return None #self recursive
+            elif term.name in done:
+                return done[term.name] #done need to do recursive
+            elif term.name in d:
+                new_term = d.pop(term.name)
 
-        eq1 = new_eq1
-        eq2 = new_eq2
+                stack.add(term.name)
+                new_term = remove_ref_term(new_term, d, done, stack)
+                stack.remove(term.name)
 
-    return (eq1, eq2)
+                done[term.name] = new_term
+                return new_term
+            else:
+                return term
+        else:
+            for i in range(len(term.arg)):
+                term.arg[i] = remove_ref_term(term.arg[i], d, done, stack)
+                if term.arg[i] is None:
+                    return None
+
+            return term
+
+    d = dict(subs)
+    done = {}
+
+    while len(d) > 0:
+        next_var = parse.Term(next(iter(d)), [])
+        if remove_ref_term(next_var, d, done, set()) is None:
+            return None
+    
+    return done
 
 def stand_term(term, idx):
     if is_var(term):
@@ -140,23 +135,8 @@ def stand_term(term, idx):
 def standardize(sen, idx):
     return parse.Sentence(imp = stand_term(sen.imp, idx), pcnd =  [stand_term(x, idx) for x in sen.pcnd])
 
-#change to a canonical name
-def term_rename(term, subs):
-    if is_var(term):
-        if term.name in subs:
-            return subs[term.name]
-        else:
-            new_term = parse.Term(name = "/" + str(len(subs)), arg = [])
-            subs[term.name] = new_term
-            return new_term 
-    else:
-        return parse.Term(name = term.name, arg = [term_rename(x, subs) for x in term.arg])
-
-def lterm_rename(lterm, subs):
-    return [term_rename(x, subs) for x in lterm]
-
-def lterm_to_string(lterm):
-    return ','.join([term_to_string(x) for x in lterm])
+def lterm_to_string(lterm, full):
+    return ','.join([term_to_string(x, full) for x in lterm])
 
 def need_quote(s):
     for c in s:
@@ -164,34 +144,46 @@ def need_quote(s):
             return True
     return False
 
-def name_to_string(term):
+def name_to_string(term, full):
     if is_var(term):
-        return term.name[1:]
+        if not full and term.name.find("//") >= 0:
+            return '_'
+        else:
+            return term.name[1:]
     elif need_quote(term.name):
         return f"'{term.name}'"
     else:
         return term.name
 
-def term_to_string(term):
+def term_to_string(term, full):
     if len(term.arg) == 0:
-        return name_to_string(term)
+        return name_to_string(term, full)
     else:
-        return name_to_string(term) + '(' + lterm_to_string(term.arg) + ')'
+        return name_to_string(term, full) + '(' + lterm_to_string(term.arg, full) + ')'
 
-def print_subs(subs):
+def print_term(term, full):
+    print(term_to_string(term, full), endl = '')
+
+def print_lterm(lterm, full):
+    print(lterm_to_string(lterm, full), end = '')
+
+def print_subs(subs, full):
     s = []
-    for x, y in zip(*subs):
-        assert(is_var(x))
-        if x.name.find("//") >= 0:
+    for x, y in subs.items():
+        x = parse.Term(x, [])
+        if not full and x.name.find("//") >= 0:
             continue
-
-        s.append(f"{name_to_string(x)} = {term_to_string(y)}")
+        
+        s.append(f"{name_to_string(x, full)} = {term_to_string(y, full)}")
     
     if len(s) == 0:
         print('yes', end = '')
     else:
         print(', '.join(s), end = '')
     sys.stdout.flush()
+
+def print_rule(rule, full):
+    print(term_to_string(rule.imp, full), " :- ", lterm_to_string(rule.pcnd, full), end = '')
 
 def is_cut(term):
     return len(term.arg) == 0 and term.name == "!"
@@ -205,6 +197,13 @@ def is_not(term):
 def is_smaller(term):
     return len(term.arg) == 2 and term.name == "smaller"
 
+def unify(eq1, eq2, subs):
+    new_subs = dict(subs)
+    if not simple_unify(eq1, eq2, new_subs):
+        return None
+
+    return remove_ref(new_subs)
+
 #return found, continue
 def backchain_ask(kb, goal, subs, depth, prove):
     global aborted
@@ -215,13 +214,13 @@ def backchain_ask(kb, goal, subs, depth, prove):
         if prove:
             return True, False
 
-        print_subs(subs)
+        print_subs(subs, False)
 
         c = getch.getch()
         print(';' if c == ';' else '.')
         return True, c == ';'
 
-    q = substitute(goal[0], subs_to_dict(subs))
+    q = substitute(goal[0], subs)
 
     if is_fail(q):
         return False, True
@@ -240,7 +239,7 @@ def backchain_ask(kb, goal, subs, depth, prove):
         return found, False
 
     if is_smaller(q):
-        if term_to_string(q.arg[0]) < term_to_string(q.arg[1]):
+        if term_to_string(q.arg[0], True) < term_to_string(q.arg[1], True):
             return backchain_ask(kb, goal[1:], subs, depth, prove)
         else:
             return False, True
@@ -248,22 +247,39 @@ def backchain_ask(kb, goal, subs, depth, prove):
     found = False
     cont = True
 
+    print('-'*40)
     for p in kb:
         if aborted:
             return found, False
 
         p = standardize(p, depth)
-        subs2 = unify([p.imp] + subs[0], [q] + subs[1], subs_to_set(subs))
-        if subs2 is not None:
-            new_goal = p.pcnd + goal[1:]
-            f, cont = backchain_ask(kb, new_goal, subs2, depth + 1, prove)
-            found = found or f
-            
-            if found and prove:
-                return True, False #already found one instance
 
-            if not cont:
-                break
+        new_subs = unify([p.imp], [q], subs)
+
+        if new_subs is None:
+            continue
+
+        new_goal = p.pcnd + goal[1:]
+
+        print('  ' * depth + "use rule: ", end = '')
+        print_rule(p, True)
+        print("\n" + '  ' * depth +  "subs: ", end = '')
+        print_subs(new_subs, True)
+        print("\n" + '  ' * depth + "goals:", end = '')
+        print_lterm(new_goal, True)
+        print()
+
+        f, cont = backchain_ask(kb, new_goal, new_subs, depth + 1, prove)
+        found = found or f
+        
+        if found and prove:
+            return True, False #already found one instance
+
+        if not cont:
+            break
+
+    print('  ' * depth + "Backtrack")
+    print('-' * 40)
 
     return found, cont
 
@@ -271,7 +287,7 @@ def inference(kb, goal):
     #backward chaining
     global aborted
     aborted = False
-    found, _ = backchain_ask(kb, goal, ([], []), 0, False)
+    found, _ = backchain_ask(kb, goal, {}, 0, False)
     if aborted:
         print("\raborted")
     elif not found:
@@ -305,6 +321,7 @@ else:
             break
         else:
             try:
+
                 goal = parse_goal(goal)
             except Exception as e:
                 print(str(e))
